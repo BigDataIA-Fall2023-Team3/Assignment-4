b17366f493dd0aa6be4708e7bce695fcf2916c81
Ruthwik Bommenahalli Gowda <ruthwik@Ruthwiks-MacBook-Air.local>
2023-11-17 03:55:01 -0500

    Usecase 4

---
+++ steps/USE_CASE_04.sql
@@ -0,0 +1,35 @@
+USE ROLE ANALYST_ROLE;
+USE WAREHOUSE A4_WH;
+
+USE DATABASE A4_DB;
+USE SCHEMA BIGDATA;
+
+
+CREATE OR REPLACE VIEW view_detailed_crime_analysis AS
+SELECT 
+    uci.INCIDENT_ID,
+    uci.OFFENSE_CATEGORY,
+    classify_crime_severity(uci.OFFENSE_CATEGORY) AS Severity_Classification,
+    gi.GEO_NAME AS Geography_Name,
+    uci.CITY AS Incident_Location,
+    uci.DATE
+FROM 
+    URBAN_CRIME_INCIDENT_LOG uci
+JOIN 
+    GEOGRAPHY_INDEX gi 
+ON 
+    uci.GEO_ID = gi.GEO_ID;
+
+
+CREATE OR REPLACE VIEW view_crime_summary AS
+SELECT
+    Incident_Location,
+    COUNT(*) AS Total_Number_of_Crimes,
+    SUM(CASE WHEN Severity_Classification = 'High' THEN 1 ELSE 0 END) AS High,
+    SUM(CASE WHEN Severity_Classification = 'Medium' THEN 1 ELSE 0 END) AS Medium,
+    SUM(CASE WHEN Severity_Classification = 'Low' THEN 1 ELSE 0 END) AS Low
+FROM
+    view_detailed_crime_analysis
+GROUP BY
+    Incident_Location;
+
---
+++ steps/USE_CASE_04_FUNCTIONS.sql
@@ -0,0 +1,35 @@
+USE ROLE ANALYST_ROLE;
+USE WAREHOUSE A4_WH;
+
+USE DATABASE A4_DB;
+USE SCHEMA BIGDATA;
+
+
+CREATE OR REPLACE FUNCTION classify_crime_severity(offense_category STRING)
+RETURNS STRING
+AS
+$$
+    CASE
+        WHEN offense_category IN ('Homicide', 'Human Trafficking', 'Sex Offense', 'Offense Involving Children') THEN 'High'
+        WHEN offense_category IN ('Robbery', 'Battery Or Assault', 'Narcotics', 'Burglary', 'Interference With Public Officer', 'Kidnapping') THEN 'Medium'
+        ELSE 'Low'
+    END
+
+$$;
+
+
+CREATE OR REPLACE FUNCTION extract_weekday(date_input DATE)
+RETURNS STRING
+AS
+$$
+    CASE DAYOFWEEK(date_input)
+        WHEN 1 THEN 'Sunday'
+        WHEN 2 THEN 'Monday'
+        WHEN 3 THEN 'Tuesday'
+        WHEN 4 THEN 'Wednesday'
+        WHEN 5 THEN 'Thursday'
+        WHEN 6 THEN 'Friday'
+        WHEN 7 THEN 'Saturday'
+    END
+$$;
+
